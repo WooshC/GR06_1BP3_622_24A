c881b805944c590f8780a38f8b80bd4406a2cd96
moises7651 <moises.arequipa@epn.edu.ec>
2024-10-21 21:44:32 -0500

    suma

--- src/main/java/clases/CalculatorExecute.java
+++ src/main/java/clases/CalculatorExecute.java
@@ -3,15 +3,16 @@ package clases;
 public class CalculatorExecute {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        if (args.length == 0) {
-            System.out.println("No se proporcionaron argumentos. Se requieren operación y números.");
-            return;
-        }
 
-        // Suponiendo que args[0] es la operación y los siguientes son los números
-        int operationChoice = Integer.parseInt(args[0]);
-        int firstNumber = Integer.parseInt(args[1]);
-        int secondNumber = Integer.parseInt(args[2]);
+        // Si se desea, se puede verificar la operación a ejecutar desde argumentos
+        int operationChoice = args.length > 0 ? Integer.parseInt(args[0]) : 1; // Por defecto, operación de suma
+
+        // Definición de los números
+        int firstNumber = 5; // Primer número
+        int secondNumber = 2; // Segundo número
+
+        // Llamada a la función de ejecución
         calculator.execute(operationChoice, firstNumber, secondNumber);
     }
 }
+
