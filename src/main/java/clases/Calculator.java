612ff691e964a2310a1532f835261b04f65a1cc2
moises7651 <moises.arequipa@epn.edu.ec>
2024-10-21 19:33:53 -0500

    Clases
    clases añadidas

---
+++ src/main/java/clases/Calculator.java
@@ -0,0 +1,7 @@
+package clases;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/clases/CalculatorTest.java
@@ -0,0 +1,7 @@
+package clases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+}
